0233c68657cd24198606d8b77934357816c09301
Ray lai <rayymlai@gmail.com>
2016-05-20 23:31:49 -0700

    update readme and buildme script

---
+++ buildme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Program: buildme.sh
+# Purpose: Example how to build quindar widgets
+# Author:  Ray Lai
+# Updated: May 20, 2016
+echo "Quindar widget build scripts"
+cd /src
+echo "...building dependencies
+npm install
