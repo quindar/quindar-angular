50aa81ce89ebbd388a9b855f7d4033fff5621e3e
Ray Lai <rayymlai@gmail.com>
2016-06-20 10:20:00 -0700

    flot example - no data source yet

--- app/controllers/index08aControllers.js
+++ app/controllers/index08aControllers.js
@@ -7,7 +7,7 @@
 // for google charts only; can be removed for quindar 
 google.load('visualization', '1', {'packages':['corechart']});
 
-var app = angular.module("app", ['gridster', 'ui.bootstrap', 'ui.router']);
+var app = angular.module("app", ['gridster', 'ui.bootstrap', 'ui.router', 'angular-flot']);
 
 // ui.router definitions
  app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -131,8 +131,8 @@ app.controller('widgetController', ['$scope','$rootScope', function($scope, $roo
 }]);
 
 // controller: dashboardControllerfor dashboard widgets
-app.controller('dashboardController', ['$scope', '$timeout', 
-  function($scope, $timeout) {
+app.controller('dashboardController', ['$scope', '$timeout', 'adminFactory',
+  function($scope, $timeout, adminFactory) {
 
     $scope.gridsterOptions = {
       margins: [20, 20],
@@ -465,6 +465,81 @@ app.controller('dashboardController', ['$scope', '$timeout',
       $scope.selectedDashboardId = nextPage;
       $scope.dashboard = $scope.dashboards[nextPage];
     };
+
+    // test flot: begin
+ $scope.dataset = [{ data: [], yaxis: 1, label: 'sin' }];
+  $scope.options = {
+    legend: {
+      container: '#legend',
+      show: true
+    }
+  };
+
+  for (var i = 0; i < 14; i += 0.5) {
+    $scope.dataset[0].data.push([i, Math.sin(i)]);
+  }
+
+  //
+  // Categories Example
+  //
+
+  $scope.categoriesDataset = [[['January', 10], ['February', 8], ['March', 4], ['April', 13], ['May', 17], ['June', 9]]];
+  $scope.categoriesOptions = {
+    series: {
+      bars: {
+        show: true,
+        barWidth: 0.6,
+        align: 'center'
+      }
+    },
+    xaxis: {
+      mode: 'categories',
+      tickLength: 0
+    }
+  };
+
+  //
+  // Pie Chart Example
+  //
+
+  $scope.pieDataset = [];
+  $scope.pieOptions = {
+    series: {
+      pie: {
+        show: true
+      }
+    }
+  };
+
+  var pieSeries = Math.floor(Math.random() * 6) + 3;
+
+  for (i = 0; i < pieSeries; i++) {
+    $scope.pieDataset[i] = {
+      label: 'Series' + (i + 1),
+      data: Math.floor(Math.random() * 100) + 1
+    };
+  }
+
+  //
+  // Event example
+  //
+
+  $scope.eventDataset = angular.copy($scope.categoriesDataset);
+  $scope.eventOptions = angular.copy($scope.categoriesOptions);
+  $scope.eventOptions.grid = {
+    clickable: true,
+    hoverable: true
+  };
+
+  $scope.onEventExampleClicked = function (event, pos, item) {
+    alert('Click! ' + event.timeStamp + ' ' + pos.pageX + ' ' + pos.pageY);
+  };
+
+  $scope.onEventExampleHover = function (event, pos, item) {
+    console.log('Hover! ' + event.timeStamp + ' ' + pos.pageX + ' ' + pos.pageY);
+  };
+
+    // test flot: end
   }
 ])
 
